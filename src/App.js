b3d5f6a9c8dbf217b8ece5552efadbf5a625fea1
Paulo <paulo.jesus2@fatec.sp.gov.br>
2022-10-25 09:34:26 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import{BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home';
+import Cadastro from './pages/Cadastro/Cadastro';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+function App() {
+  return (
+      <>
+      <BrowserRouter>
+        <NavBar />
+        <h1>LAB DEV WEB</h1>
+        <main className='container'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/cadastro' element={<Cadastro />} />
+        </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <h3>LAB DEV WEB</h3>
+    <p>Fatec DSM &copy; 2022</p>
+    </footer>
+  )
+}
+
+export default Footer
