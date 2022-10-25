dd9527e339b4cf8182b33b8e6a88c51ccbb8b7d2
Paulo <paulo.jesus2@fatec.sp.gov.br>
2022-10-25 10:23:27 -0300

    add FireBase authentication

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCC4nG-b8L7QxW4X8m06d41dq2mbp-SUA",
+  authDomain: "labwebdsm.firebaseapp.com",
+  projectId: "labwebdsm",
+  storageBucket: "labwebdsm.appspot.com",
+  messagingSenderId: "572783695028",
+  appId: "1:572783695028:web:ca9e56d5d3f0749a2f73e1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
